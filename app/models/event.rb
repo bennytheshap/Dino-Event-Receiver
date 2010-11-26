436a7a862159ef5498cded90d87aa411b9937933
Ben Shapiro <bshapiro@morgridgeinstitute.org>
2010-11-26 10:12:06 -0600

    simple event type

---
+++ app/models/event.rb
@@ -0,0 +1,14 @@
+require 'randomly_sharded_mongomapper_document'
+
+class Event
+  include MongoMapper::Document
+  plugin RandomShardPlugin
+  
+  key :session_id, ObjectId, :required=>true
+  key :time, Numeric, :required=>true
+  key :x, Numeric, :required=>true
+  key :y, Numeric, :required=>true
+  
+  belongs_to :session
+end
+
